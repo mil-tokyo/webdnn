f487bec87ef118234f0693d89539086e218cc435
Kikurage <y.kikura@gmail.com>
2017-04-21 17:18:35 +0900

    fix generator

--- src/graph_builder/backend/fallback/generator.py
+++ src/graph_builder/backend/fallback/generator.py
@@ -16,6 +16,7 @@ from graph_builder.backend.fallback.allocator import Allocator, MemoryLayout
 from graph_builder.backend.fallback.graph_descriptor import GraphDescriptor
 from graph_builder.backend.fallback.kernel import Kernel
 from graph_builder.graph import Operator, operators as O
+from graph_builder.optimizer import util
 from graph_builder.util import flags
 
 
@@ -37,61 +38,41 @@ def generate(graph: Operator) -> Tuple[GraphDescriptor, np.array]:
     return descriptor, constants_data
 
 
-def generate_kernels(op: Operator, constants_layout: MemoryLayout, variables_layout: MemoryLayout) -> List[Kernel]:
-    if isinstance(op, O.Compose):
-        kernels = generate_kernel_compose(op, constants_layout, variables_layout)
-
-    elif isinstance(op, O.Linear):
-        kernels = K.linear(op)
-
-    elif isinstance(op, O.AxiswiseBias):
-        kernels = K.axiswise_bias(op)
-
-    elif isinstance(op, O.AxiswiseScale):
-        kernels = K.axiswise_scale(op)
-
-    elif isinstance(op, O.Convolution2D):
-        kernels = K.convolution_2d(op)
-
-    elif isinstance(op, O.MaxPooling2D):
-        kernels = K.max_pooling_2d(op)
-
-    elif isinstance(op, O.AveragePooling2D):
-        kernels = K.average_pooling_2d(op)
-
-    elif isinstance(op, O.ElementwiseSum):
-        kernels = K.elementwise_sum(op)
-
-    elif isinstance(op, O.Flatten):
-        kernels = K.flatten(op)
-
-    elif isinstance(op, O.Relu):
-        kernels = K.relu(op)
-
-    else:
-        raise NotImplementedError(f"{op} is Unknown for Fallback Descriptor Generator")
-
-    return kernels
-
-
-def generate_kernel_compose(op: O.Compose, constants_layout: MemoryLayout, variables_layout: MemoryLayout) -> List[Kernel]:
+def generate_kernels(graph: Operator, constants_layout: MemoryLayout, variables_layout: MemoryLayout) -> List[Kernel]:
     kernels: List[Kernel] = []
-    op_queue: List[Operator] = []
-    op_checked: Set[Operator] = set()
-
-    for var in op.outputs_alias:
-        op_queue.append(var.output_from)
-
-    while len(op_queue) > 0:
-        op = op_queue.pop(0)
-        if op in op_checked:
+    for op in util.listup_operator_in_order(graph):
+        if isinstance(op, O.Compose):
+            # kernels = generate_kernel_compose(op, constants_layout, variables_layout)
             continue
-        op_checked.add(op)
 
-        sub_kernels = generate_kernels(op, constants_layout, variables_layout)
-        kernels = sub_kernels + kernels
-        for v in op.inputs.values():
-            if v.output_from is not None:
-                op_queue.append(v.output_from)
+        elif isinstance(op, O.Linear):
+            kernels += K.linear(op)
+
+        elif isinstance(op, O.AxiswiseBias):
+            kernels += K.axiswise_bias(op)
+
+        elif isinstance(op, O.AxiswiseScale):
+            kernels += K.axiswise_scale(op)
+
+        elif isinstance(op, O.Convolution2D):
+            kernels += K.convolution_2d(op)
+
+        elif isinstance(op, O.MaxPooling2D):
+            kernels += K.max_pooling_2d(op)
+
+        elif isinstance(op, O.AveragePooling2D):
+            kernels += K.average_pooling_2d(op)
+
+        elif isinstance(op, O.ElementwiseSum):
+            kernels += K.elementwise_sum(op)
+
+        elif isinstance(op, O.Flatten):
+            kernels += K.flatten(op)
+
+        elif isinstance(op, O.Relu):
+            kernels += K.relu(op)
+
+        else:
+            raise NotImplementedError(f"{op} is Unknown for Fallback Descriptor Generator")
 
     return kernels
