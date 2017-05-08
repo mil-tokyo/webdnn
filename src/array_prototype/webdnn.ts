b3c416c96ba577bc98d15ac536bfb648032864c2
Masatoshi Hidaka <hidaka@mi.t.u-tokyo.ac.jp>
2017-05-08 21:00:03 +0900

    support fallback on weight loading failure

--- src/array_prototype/webdnn.ts
+++ src/array_prototype/webdnn.ts
@@ -6,58 +6,80 @@
 namespace WebDNN {
     export let gpu: GPUInterface;
 
+    let givenBackendOptions: { [key: string]: any };
+    let tryingBackendOrder: string[];
+    let loadedBackendName: string;
+
+    async function tryInitNext(): Promise<string> {
+        let backend_name = tryingBackendOrder.shift();
+        if (!backend_name) {
+            throw new Error('No backend is available');
+        }
+
+        let option = givenBackendOptions[backend_name];
+        let gpuif: GPUInterface;
+        try {
+            switch (backend_name) {
+                case 'webgpu':
+                    gpuif = new GPUInterfaceWebGPU(option);
+                    break;
+                case 'webassembly':
+                    gpuif = new GPUInterfaceWebassembly(option);
+                    break;
+                case 'fallback':
+                    gpuif = new GPUInterfaceFallback(option);
+                    break;
+                default:
+                    throw new Error('Unknown backend ' + backend_name);
+            }
+            await gpuif.init();
+            gpu = gpuif;
+            loadedBackendName = backend_name;
+        } catch (ex) {
+            console.error(`Failed to initialize ${backend_name} backend: ${ex}`);
+            return await tryInitNext();
+        }
+
+        return loadedBackendName;
+    }
+
     export async function init(backendOrder?: string | string[], backendOptions: { [key: string]: any } = {}): Promise<string> {
-        let loaded_backend_name = '';
         if (!backendOrder) {
             backendOrder = ['webgpu', 'webassembly'];
         } else if (typeof backendOrder === 'string') {
             backendOrder = [backendOrder];
         }
 
-        let backend_order_with_fb = backendOrder.concat(backendOrder, ['fallback']);
-        for (let i = 0; i < backend_order_with_fb.length; i++) {
-            let backend_name = backend_order_with_fb[i];
-            let option = backendOptions[backend_name];
-            let gpuif: GPUInterface;
-            try {
-                switch (backend_name) {
-                    case 'webgpu':
-                        gpuif = new GPUInterfaceWebGPU(option);
-                        break;
-                    case 'webassembly':
-                        gpuif = new GPUInterfaceWebassembly(option);
-                        break;
-                    case 'fallback':
-                        gpuif = new GPUInterfaceFallback(option);
-                        break;
-                    default:
-                        throw new Error('Unknown backend ' + backend_name);
-                }
-                await gpuif.init();
-                gpu = gpuif;
-                loaded_backend_name = backend_name;
-                break;
-            } catch (ex) {
-                console.error(`Failed to initialize ${backend_name} backend: ${ex}`);
-            }
-        }
+        givenBackendOptions = backendOptions;
+        tryingBackendOrder = backendOrder.concat(backendOrder, ['fallback']);
 
-        return loaded_backend_name;
+        await tryInitNext();
+
+        return loadedBackendName;
     }
 
     /*
     Prepare backend interface and load model data at once
     */
     export async function prepareAll(directory: string, backend_order?: string | string[], backendOptions?: { [key: string]: any }): Promise<DNNInterface> {
-        let backend_name = await init(backend_order, backendOptions);
-        let runner = gpu.createDNNDescriptorRunner();
-        await runner.load(directory);
-        let input_views = await runner.getInputViews();
-        let output_views = await runner.getOutputViews();
-        return {
-            backendName: backend_name, inputViews: input_views,
-            outputViews: output_views, run: runner.run.bind(runner)
-        };
+        await init(backend_order, backendOptions);
+
+        while (true) {
+            try {
+                let runner = gpu.createDNNDescriptorRunner();
+                await runner.load(directory);
+                let input_views = await runner.getInputViews();
+                let output_views = await runner.getOutputViews();
+
+                return {
+                    backendName: loadedBackendName, inputViews: input_views,
+                    outputViews: output_views, run: runner.run.bind(runner)
+                };
+            } catch (ex) {
+                console.error(`Model loading failed for ${loadedBackendName} backend. Trying next backend. ${ex.message}`);
+                await tryInitNext();
+            }
+        }
     }
 
     export interface DNNInterface {
--- src/graph_builder/backend/webassembly/webassembly_header.js
+++ src/graph_builder/backend/webassembly/webassembly_header.js
@@ -4,29 +4,37 @@ var Module = {};
 onmessage = function (event) {
     switch (event.data.type) {
         case 'run':
-            var data_offset = Module._get_data_buffer();
-            for (var i = 0; i < event.data.inputs.length; i++) {
-                var var_alloc = event.data.inputs[i];
-                var data_buf = new Float32Array(Module.buffer, data_offset + var_alloc.offset * Float32Array.BYTES_PER_ELEMENT, var_alloc.size);
-                data_buf.set(var_alloc.data);
+            try {
+                var data_offset = Module._get_data_buffer();
+                for (var i = 0; i < event.data.inputs.length; i++) {
+                    var var_alloc = event.data.inputs[i];
+                    var data_buf = new Float32Array(Module.buffer, data_offset + var_alloc.offset * Float32Array.BYTES_PER_ELEMENT, var_alloc.size);
+                    data_buf.set(var_alloc.data);
+                }
+                Module._run();
+                var outputs = [];
+                for (var i = 0; i < event.data.outputs.length; i++) {
+                    var var_alloc = event.data.outputs[i];
+                    var data_buf = new Float32Array(Module.buffer, data_offset + var_alloc.offset * Float32Array.BYTES_PER_ELEMENT, var_alloc.size);
+                    outputs.push(data_buf);
+                }
+                postMessage(outputs);
+            } catch (ex) {
+                postMessage(ex.message);
             }
-            Module._run();
-            var outputs = [];
-            for (var i = 0; i < event.data.outputs.length; i++) {
-                var var_alloc = event.data.outputs[i];
-                var data_buf = new Float32Array(Module.buffer, data_offset + var_alloc.offset * Float32Array.BYTES_PER_ELEMENT, var_alloc.size);
-                outputs.push(data_buf);
-            }
-            postMessage(outputs);
             break;
         case 'weight':
-            var weight_offset = Module._get_weight_buffer();
-            var weight_buf = new Float32Array(Module.buffer, weight_offset, event.data.data.length);
-            weight_buf.set(event.data.data);
-            postMessage('weight');
+            try {
+                var weight_offset = Module._get_weight_buffer();
+                var weight_buf = new Float32Array(Module.buffer, weight_offset, event.data.data.length);
+                weight_buf.set(event.data.data);
+                postMessage(0);
+            } catch (ex) {
+                postMessage(ex.message);
+            }
             break;
         case 'init':
-            postMessage('init');
+            postMessage(0);
             break;
     }
 };
