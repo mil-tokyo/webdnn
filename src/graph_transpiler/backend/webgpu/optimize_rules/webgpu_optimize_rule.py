67d730b8eebc026b9fe08d4bc2790799539bd9ba
Kikurage <y.kikura@gmail.com>
2017-05-14 11:07:26 +0900

    add Linear optimization

--- src/graph_transpiler/backend/webgpu/optimize_rules/webgpu_optimize_rule.py
+++ src/graph_transpiler/backend/webgpu/optimize_rules/webgpu_optimize_rule.py
@@ -1,6 +1,7 @@
 from graph_transpiler.backend.webgpu.optimize_rules.optimize_convolution2d import OptimizeConvolution2D
 from graph_transpiler.backend.webgpu.optimize_rules.optimize_deconvolution2d import OptimizeDeconvolution2D
 from graph_transpiler.backend.webgpu.optimize_rules.optimize_elementwise import OptimizeElementwise
+from graph_transpiler.backend.webgpu.optimize_rules.optimize_linear import OptimizeLinear
 from graph_transpiler.graph.optimize_rule import OptimizeRule
 
 
@@ -11,3 +12,4 @@ class WebGPUOptimizeRule(OptimizeRule):
         self.register(OptimizeConvolution2D())
         self.register(OptimizeDeconvolution2D())
         self.register(OptimizeElementwise())
+        self.register(OptimizeLinear())
