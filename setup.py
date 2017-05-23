92158852781277d26229a59ed44ec2af6987c5e2
Kikurage <y.kikura@gmail.com>
2017-05-23 11:18:34 +0900

    change directory structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="webdnn",
     version="1.0.0",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
+    package_dir={"": "src/graph_transpiler"},
+    packages=find_packages("src/graph_transpiler"),
     package_data={"": "*.js"}
 )
