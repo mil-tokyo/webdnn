c0965a4cba5f8ded770b39505abd2b262d0a6f9f
Kikurage <y.kikura@gmail.com>
2017-05-23 15:35:44 +0900

    add getBackendAvailability()

---
+++ src/descriptor_runner/get_backend_availability.ts
@@ -0,0 +1,32 @@
+namespace WebDNN {
+    function getBackendAvailabilityWebGPU() {
+        return 'WebGPUComputeCommandEncoder' in window;
+    }
+
+    function getBackendAvailabilityWebAssembly() {
+        return 'WebAssembly' in window;
+    }
+
+    function getBackendAvailabilityFallback() {
+        return true;
+    }
+
+    /**
+     * Check backend availability
+     * @returns List of backend availability and default selected backend order
+     */
+    export function getBackendAvailability() {
+        let status: { [name: string]: boolean } = {
+            'webgpu': getBackendAvailabilityWebGPU(),
+            'webassembly': getBackendAvailabilityWebAssembly(),
+            'fallback': getBackendAvailabilityFallback()
+        };
+
+        let order = ['webgpu', 'webassembly', 'fallback'].filter(backend => status[backend]);
+
+        return {
+            status: status,
+            defaultOrder: order
+        }
+    }
+}
