ff9ecd0cc36f9a04b7efa1a58f6bc70b642a41e1
Kikurage <y.kikura@gmail.com>
2017-06-05 17:07:23 +0900

    remove unused interface

--- src/descriptor_runner/graph_descriptor/graph_descriptor.ts
+++ src/descriptor_runner/graph_descriptor/graph_descriptor.ts
@@ -14,21 +14,7 @@ namespace WebDNN {
         outputs: string[];
 
         memory_layout: MemoryLayout,
-
-        /**
-         * Allocation information for each variable.
-         */
-        weight_allocation: {
-            allocations: { [name: string]: any }
-        };
-
-        /**
-         * Allocation information for each variable.
-         */
-        variable_allocation: {
-            allocations: { [name: string]: any }
-        };
-
+        
         /**
          * Encoding algorithm of weight binary data.
          */
