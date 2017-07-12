0dd7e2bbeacfbdc1a26d65628293ec2effe8e54d
Masatoshi Hidaka <hidaka@mi.t.u-tokyo.ac.jp>
2017-07-12 22:37:02 +0900

    change transpose operator to use elementwise

--- src/graph_transpiler/webdnn/graph/operators/transpose.py
+++ src/graph_transpiler/webdnn/graph/operators/transpose.py
@@ -4,8 +4,7 @@ import numpy as np
 
 from webdnn.graph.axis import Axis
 from webdnn.graph.operator import Operator
-from webdnn.graph.operators.attributes.elementwise import Elementwise
-from webdnn.graph.operators.attributes.inplace import Inplace
+from webdnn.graph.operators.elementwise import Elementwise
 from webdnn.graph.order import Order
 from webdnn.graph.variable import Variable
 
@@ -14,7 +13,7 @@ from webdnn.graph.variable import Variable
 from webdnn.util.misc import mul
 
 
-class Transpose(Operator):
+class Transpose(Elementwise):
     """Transposition. Doing nothing in frontend level,
     and do memory transposition in backend if input / output variable order differs.
     This layer is inserted in optimizer to support layers which accepts certain order.
@@ -22,23 +21,4 @@ class Transpose(Operator):
     Args:
         name (str): Operator name.
     """
-
-    def __init__(self, name: Optional[str]):
-
-        super().__init__(name)
-
-        self.attributes = {Elementwise(self)}
-
-    def __call__(self, x: Variable):
-        """
-        Args:
-            x (:class:`~webdnn.graph.variable.Variable`): Input
-
-        Returns:
-            tuple of :class:`~webdnn.graph.variable.Variable`: Output
-        """
-        y = Variable(x.shape, x.order)
-        self.append_input("x0", x)
-        self.append_output("y", y)
-
-        return y,
+    pass
