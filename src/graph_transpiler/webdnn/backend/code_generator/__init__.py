20d41140bdf26470379fddf2e6efc65a1ee37a6d
Kikurage <y.kikura@gmail.com>
2017-07-30 09:41:09 +0900

    #417 fix __init__.py import list

--- src/graph_transpiler/webdnn/backend/code_generator/__init__.py
+++ src/graph_transpiler/webdnn/backend/code_generator/__init__.py
@@ -1,3 +1,5 @@
 from webdnn.backend.code_generator import allocator
+from webdnn.backend.code_generator import command_buffer
 from webdnn.backend.code_generator import injector
 from webdnn.backend.code_generator import injectors
+from webdnn.backend.code_generator import templates
