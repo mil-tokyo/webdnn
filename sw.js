1e44ddf9c712346f37b2e01b1019548dc73e09d7
Kikurage <y.kikura@gmail.com>
2017-07-31 16:51:59 +0900

    update

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('/webdnn/sw-toolbox.js');
 
 toolbox.precache(['/',
-	'https://mil-tokyo.github.io/webdnn-data/models/resnet50/weight_webassembly.bin?raw=true&v=4',
-	'https://mil-tokyo.github.io/webdnn-data/models/resnet50/weight_webgpu.bin?raw=true&v=4',
+	'https://mil-tokyo.github.io/webdnn-data/models/resnet/weight_webassembly.bin?raw=true&v=4',
+	'https://mil-tokyo.github.io/webdnn-data/models/resnet/weight_webgpu.bin?raw=true&v=4',
 	'https://mil-tokyo.github.io/webdnn-data/models/neural_style_transfer/weight_webassembly.bin?raw=true&v=4',
 	'https://mil-tokyo.github.io/webdnn-data/models/neural_style_transfer/weight_webgpu.bin?raw=true&v=4'
 ]);
