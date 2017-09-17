82840487dc357b1708573d61e9ebf3b54389e891
Masatoshi Hidaka <hidaka@mi.t.u-tokyo.ac.jp>
2017-09-17 14:20:40 +0900

    avoid NaN of tanh in fast math mode

--- src/graph_transpiler/webdnn/backend/webgpu/kernels/tanh.py
+++ src/graph_transpiler/webdnn/backend/webgpu/kernels/tanh.py
@@ -1,4 +1,10 @@
 from webdnn.backend.webgpu.kernels.elementwise import register_elementwise_kernel
 from webdnn.graph.operators.tanh import Tanh
 
-register_elementwise_kernel(Tanh, "y = tanh(x0);")
+# In fast math mode, tanh(x) is implemented as
+# (t – 1.0)/(t + 1.0) where t = exp(2.0 * x)
+# (from Metal Shading Language Specification)
+# and it easily overflow and produces NaN when x is large (negative large x is ok).
+# When abs(x) >= 10.0, tanh will saturate in float precision,
+# so output the constant value.
+register_elementwise_kernel(Tanh, "y = (x0 >= 10.0 ? 1.0 : tanh(x0));")
