7764534b872ec52b8c362285322872bcea4b79e3
Kikurage <y.kikura@gmail.com>
2017-10-03 19:59:13 +0900

    add warning about softmax

--- src/graph_transpiler/webdnn/backend/webgl/optimize_rules/webgl_optimize_rule.py
+++ src/graph_transpiler/webdnn/backend/webgl/optimize_rules/webgl_optimize_rule.py
@@ -5,9 +5,13 @@ from webdnn.backend.webgl.optimize_rules.replace_linear_by_sgemm import ReplaceL
 from webdnn.graph.graph import Graph
 from webdnn.graph.operators.softmax import Softmax
 from webdnn.graph.optimize_rule import OptimizeRule
+from webdnn.util import console
 
 
 class RemoveLastSoftmax(OptimizeRule):
+    def __init__(self):
+        self.flag_warning = True
+
     def optimize(self, graph: Graph):
         flag_changed = False
         for v in graph.outputs:
@@ -19,6 +23,12 @@ class RemoveLastSoftmax(OptimizeRule):
             y = op.outputs["y"]
             if y.order != x.order:
                 x.change_order(y.order)
+
+            if self.flag_warning:
+                self.flag_warning = False
+                console.warning(
+                    "Currently, WebGL backend doesn't support SoftMax, therefore you have to apply softmax function in JavaScript. "
+                    "Please see (https://github.com/mil-tokyo/webdnn/issues/588#issuecomment-333807690)")
 
             op.remove_all()
             index = graph.outputs.index(y)
