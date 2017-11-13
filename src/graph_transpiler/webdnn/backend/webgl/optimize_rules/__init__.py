ec24867868dc5b7e774ee42f07686c01303abf05
Kikurage <y.kikura@gmail.com>
2017-11-13 21:58:06 +0900

    fix test

--- src/graph_transpiler/webdnn/backend/webgl/optimize_rules/__init__.py
+++ src/graph_transpiler/webdnn/backend/webgl/optimize_rules/__init__.py
@@ -4,7 +4,6 @@ from webdnn.backend.webgl.optimize_rules import fix_sgemm_texture_shape
 from webdnn.backend.webgl.optimize_rules import fix_tensordot_texture_shape
 from webdnn.backend.webgl.optimize_rules import insert_channel_mode_conversion
 from webdnn.backend.webgl.optimize_rules import insert_transpose
-from webdnn.backend.webgl.optimize_rules import replace_tensordot_by_sgemm
 from webdnn.backend.webgl.optimize_rules import simplify_channel_mode_conversion
 from webdnn.backend.webgl.optimize_rules import split_texture
 from webdnn.backend.webgl.optimize_rules import webgl_optimize_rule
