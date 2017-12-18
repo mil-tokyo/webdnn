62cf7cdeeeb83b3413b230ebf0923836c194927d
Kiikurage <y.kikura@gmail.com>
2017-12-18 17:06:14 +0900

    fix cover_all mode setting in TF

--- test/runtime/frontend_test/keras_test/layers_test/pooling_test/average_pooling_2d_test.py
+++ test/runtime/frontend_test/keras_test/layers_test/pooling_test/average_pooling_2d_test.py
@@ -5,12 +5,12 @@ from test.util import generate_kernel_test_case, wrap_template
 
 
 @wrap_template
-def template(pool_size=(3, 3), strides=(2, 2), padding="valid", data_format=None, description: str = ""):
-    x = keras.layers.Input((15, 17, 16))  # (height + pad * 2 - pool_size) % stride == 0 to avoid edge difference
+def template(pool_size=(3, 3), shape=(15, 17, 16), strides=(2, 2), padding="valid", data_format=None, description: str = ""):
+    x = keras.layers.Input(shape)
     y = keras.layers.AveragePooling2D(pool_size=pool_size, strides=strides, padding=padding, data_format=data_format)(x)
     model = keras.models.Model([x], [y])
 
-    vx = np.random.rand(2, 15, 17, 16)
+    vx = np.random.rand(2, *shape)
     vy = model.predict(vx, batch_size=2)
 
     graph = KerasConverter(batch_size=2, use_tensorflow_converter=False).convert(model)
@@ -39,6 +39,11 @@ def test_channels_first():
 def test_padding_valid():
     template(padding="valid")
 
+
 # FIXME: Not supported yet
 # def test_padding_same():
 #     template(padding="same")
+
+
+def test_no_cover_all():
+    template(pool_size=2, shape=(2, 2, 5), strides=2, padding="SAME")
