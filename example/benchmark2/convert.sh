713bd990a9dee3a868274171235604af0bacac79
zhaoming0 <mingx.zhao@intel.com>
2017-12-28 08:43:38 -0500

    Modifid example/benchmark/convert.sh and example/benchmark2/convert.sh by changing python to python3, correcting git command with "git reset".

--- example/benchmark2/convert.sh
+++ example/benchmark2/convert.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo "Generate Keras model"
-python generate_keras_models.py
+python3 generate_keras_models.py
 
 echo ""
 echo "Encode Keras model into WebDNN model:"
-OPTIMIZE=1 python ../../bin/convert_keras.py keras_model/model.h5 \
+OPTIMIZE=1 python3 ../../bin/convert_keras.py keras_model/model.h5 \
     --input_shape '(1,224,224,3)' \
     --out webdnn_model
