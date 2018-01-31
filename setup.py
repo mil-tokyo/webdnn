0047c3cfd01a635c48ce318e90495d85166b9339
Masatoshi Hidaka <hidaka@mi.t.u-tokyo.ac.jp>
2018-01-31 18:27:06 +0900

    support pip distribution

--- setup.py
+++ setup.py
@@ -8,12 +8,25 @@ import json
 from setuptools import setup, find_packages
 
 with open("./package.json") as f:
-    version = json.load(f)["version"]
+    package_info = json.load(f)
 
 setup(
     name="webdnn",
-    version=version,
+    version=package_info["version"],
+    python_requires=">=3.6",
     package_dir={"": "src/graph_transpiler"},
     packages=find_packages("src/graph_transpiler"),
-    package_data={"": "*.js"}, install_requires=['numpy']
+    package_data={"": "*.js"}, install_requires=['numpy'],
+    url="https://github.com/mil-tokyo/webdnn",
+    description=package_info["description"],
+    author=package_info["author"]["name"],
+    author_email=package_info["author"]["email"],
+    keywords=" ".join(package_info["keywords"]),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6"
+        ]
 )
