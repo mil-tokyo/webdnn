2d2ba5df6fca4fa5175173fd4a1f475ef5311210
Masatoshi Hidaka <hidaka@edgeintelligence.jp>
2021-09-16 16:05:11 +0900

    refactor unary on wasm

--- src/shader/wasm/src/kernels/standard/unarys.cpp
+++ src/shader/wasm/src/kernels/standard/unarys.cpp
@@ -5,38 +5,14 @@
 
 extern "C"
 {
-  void WEBDNN_KERNEL kernel_ceil(const float *src, float *dst, int length)
-  {
-    webdnn_unary(src, dst, length, [](float s) { return std::ceil(s); });
-  }
-
-  void WEBDNN_KERNEL kernel_exp(const float *src, float *dst, int length)
-  {
-    webdnn_unary(src, dst, length, [](float s) { return std::exp(s); });
-  }
-
-  void WEBDNN_KERNEL kernel_floor(const float *src, float *dst, int length)
-  {
-    webdnn_unary(src, dst, length, [](float s) { return std::floor(s); });
-  }
-
-  void WEBDNN_KERNEL kernel_relu(const float *src, float *dst, int length)
-  {
-    webdnn_unary(src, dst, length, [](float s) { return std::max(s, 0.0f); });
-  }
-
-  void WEBDNN_KERNEL kernel_sigmoid(const float *src, float *dst, int length)
-  {
-    webdnn_unary(src, dst, length, [](float s) { return (std::tanh(s * 0.5f) + 1.0f) * 0.5f; });
-  }
-
-  void WEBDNN_KERNEL kernel_sqrt(const float *src, float *dst, int length)
-  {
-    webdnn_unary(src, dst, length, [](float s) { return std::sqrt(s); });
-  }
-
-  void WEBDNN_KERNEL kernel_tanh(const float *src, float *dst, int length)
-  {
-    webdnn_unary(src, dst, length, [](float s) { return std::tanh(s); });
-  }
+  
+#define DEFINE_UNARY(name, func) \
+void WEBDNN_KERNEL kernel_##name(const float *src, float *dst, int length) { webdnn_unary(src, dst, length, func); }
+  DEFINE_UNARY(ceil, [](float s) { return std::ceil(s); });
+  DEFINE_UNARY(exp, [](float s) { return std::exp(s); });
+  DEFINE_UNARY(floor, [](float s) { return std::floor(s); });
+  DEFINE_UNARY(relu, [](float s) { return std::max(s, 0.0f); });
+  DEFINE_UNARY(sigmoid, [](float s) { return (std::tanh(s * 0.5f) + 1.0f) * 0.5f; });
+  DEFINE_UNARY(sqrt, [](float s) { return std::sqrt(s); });
+  DEFINE_UNARY(tanh, [](float s) { return std::tanh(s); });
 }
